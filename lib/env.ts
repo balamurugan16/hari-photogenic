fa6f093525b512762eb9e5a2733971ba21247a4e
Balamurugan D <balamurugan.07@outlook.com>
2024-05-05 19:39:28 +0530

    Changes in env

--- lib/env.ts
+++ lib/env.ts
@@ -5,19 +5,19 @@ export const env = createEnv({
   server: {
     CLOUDINARY_API_KEY: z.string(),
     CLOUDINARY_API_SECRET: z.string(),
-    CLOUDINARY_FOLDER: z.string()
+    CLOUDINARY_FOLDER: z.string(),
+    SUPABASE_URL: z.string().url(),
+    SUPABASE_ANON_KEY: z.string(),
   },
   client: {
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string(),
-    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
   },
   runtimeEnv: {
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
     CLOUDINARY_FOLDER: process.env.CLOUDINARY_FOLDER,
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
   },
 });
--- lib/supabase/supabase.ts
+++ lib/supabase/supabase.ts
@@ -3,8 +3,8 @@ import { env } from "../env"
 import { Database } from "./db.types"
 
 export const supabase = createServerClient<Database>(
-  env.NEXT_PUBLIC_SUPABASE_URL,
-  env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  env.SUPABASE_URL,
+  env.SUPABASE_ANON_KEY,
   {
     cookies: {}
   }
