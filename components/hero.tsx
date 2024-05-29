48beea1e2cf2912e4a362f3af7420091ceeb61ab
Balamurugan D <balamurugan.07@outlook.com>
2024-05-29 08:59:10 +0530

    home page highlights completed

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,64 +8,50 @@ import {
 import Image from "next/image";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
+const highlightImages = [
+  {
+    src: "/highlight-images/highlight_1.jpg",
+    alt: "wedding photo 1",
+  },
+  {
+    src: "/highlight-images/highlight_2.jpg",
+    alt: "wedding photo 2",
+  },
+  {
+    src: "/highlight-images/highlight_3.jpg",
+    alt: "wedding photo 1",
+  },
+  {
+    src: "/highlight-images/highlight_4.jpg",
+    alt: "wedding photo 4",
+  },
+  {
+    src: "/highlight-images/highlight_5.jpg",
+    alt: "wedding photo 5",
+  },
+];
+
 export function Hero() {
   return (
     <div className="w-full mx-auto ">
       <div className="relative group">
         <Carousel className="w-full">
           <CarouselContent>
-            <CarouselItem>
-              <Image
-                alt="Wedding Photo 1"
-                className="w-full rounded-lg h-[500px] sm:h-[600px] lg:h-[700px] object-cover object-center"
-                height={800}
-                src="https://picsum.photos/800/1200"
-                style={{
-                  aspectRatio: "1200/800",
-                  objectFit: "cover",
-                }}
-                width={1200}
-              />
-            </CarouselItem>
-            <CarouselItem>
-              <Image
-                alt="Wedding Photo 2"
-                className="w-full h-[500px] sm:h-[600px] lg:h-[700px] object-cover object-center"
-                height={800}
-                src="https://picsum.photos/800/1200"
-                style={{
-                  aspectRatio: "1200/800",
-                  objectFit: "cover",
-                }}
-                width={1200}
-              />
-            </CarouselItem>
-            <CarouselItem>
-              <Image
-                alt="Wedding Photo 3"
-                className="w-full h-[500px] sm:h-[600px] lg:h-[700px] object-cover object-center"
-                height={800}
-                src="https://picsum.photos/800/1200"
-                style={{
-                  aspectRatio: "1200/800",
-                  objectFit: "cover",
-                }}
-                width={1200}
-              />
-            </CarouselItem>
-            <CarouselItem>
-              <Image
-                alt="Wedding Photo 4"
-                className="w-full h-[500px] sm:h-[600px] lg:h-[700px] object-cover object-center"
-                height={800}
-                src="https://picsum.photos/800/1200"
-                style={{
-                  aspectRatio: "1200/800",
-                  objectFit: "cover",
-                }}
-                width={1200}
-              />
-            </CarouselItem>
+            {highlightImages.map((image, id) => (
+              <CarouselItem key={id}>
+                <Image
+                  alt={image.alt}
+                  className="w-full rounded-lg h-[500px] sm:h-[600px] lg:h-[700px] object-cover object-center"
+                  height={800}
+                  src={image.src}
+                  style={{
+                    aspectRatio: "1200/800",
+                    objectFit: "cover",
+                  }}
+                  width={1200}
+                />
+              </CarouselItem>
+            ))}
           </CarouselContent>
           <CarouselPrevious className="absolute top-1/2 -translate-y-1/2 left-4 z-10  ">
             <ChevronLeftIcon className="h-8 w-8" />
